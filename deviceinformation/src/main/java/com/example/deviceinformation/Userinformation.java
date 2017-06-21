59152274b03a1b214a1e151c22f8713fa8e24ee5
Ramkumar810 <ramkumar.perumal@adyog.com>
2017-06-21 11:51:32 +0530

    Version

--- deviceinformation/src/main/java/com/example/deviceinformation/Userinformation.java
+++ deviceinformation/src/main/java/com/example/deviceinformation/Userinformation.java
@@ -2,6 +2,7 @@ package com.example.deviceinformation;
 
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by kannas on 6/21/2017.
@@ -11,9 +12,10 @@ public class Userinformation extends AppCompatActivity {
     public static String androidDeviceId;
 
     public  void userIp(String name){
-        androidDeviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(),
+        name = Settings.Secure.getString(getApplicationContext().getContentResolver(),
                 Settings.Secure.ANDROID_ID);
-        name=androidDeviceId;
+        Log.d("name",name+"");
+        //name=androidDeviceId;
     }
 
 }
