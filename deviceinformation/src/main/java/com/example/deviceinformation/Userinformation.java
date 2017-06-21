2930f7bc6e81a2632be7877ccc725dc2d457381b
Ramkumar810 <ramkumar.perumal@adyog.com>
2017-06-21 12:50:32 +0530

    info

--- deviceinformation/src/main/java/com/example/deviceinformation/Userinformation.java
+++ deviceinformation/src/main/java/com/example/deviceinformation/Userinformation.java
@@ -1,5 +1,6 @@
 package com.example.deviceinformation;
 
+import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,6 +17,16 @@ public class Userinformation extends AppCompatActivity {
         name1 = Settings.Secure.getString(getApplicationContext().getContentResolver(),
                 Settings.Secure.ANDROID_ID);
        //
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        int version = Build.VERSION.SDK_INT;
+        String versionRelease = Build.VERSION.RELEASE;
+
+        Log.e("MyActivity", "manufacturer " + manufacturer
+                + " \n model " + model
+                + " \n version " + version
+                + " \n versionRelease " + versionRelease
+        );
         Log.d("name",name1+"");
         return name1;
 
