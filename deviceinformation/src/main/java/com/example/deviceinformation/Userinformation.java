58d1437918203a7db6664ae1538cc8a394d0c842
Ramkumar810 <ramkumar.perumal@adyog.com>
2017-06-21 13:46:46 +0530

    deviceInformation

--- deviceinformation/src/main/java/com/example/deviceinformation/Userinformation.java
+++ deviceinformation/src/main/java/com/example/deviceinformation/Userinformation.java
@@ -1,9 +1,22 @@
 package com.example.deviceinformation;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.BatteryManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+
+import java.util.Locale;
 
 /**
  * Created by kannas on 6/21/2017.
@@ -11,6 +24,13 @@ import android.util.Log;
 
 public class Userinformation extends AppCompatActivity {
     public static String androidDeviceId;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        userIp();
+        this.registerReceiver(this.batteryInfoReceiver,	new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
 
     public void userIp(){
       /*  String name1;
@@ -22,15 +42,57 @@ public class Userinformation extends AppCompatActivity {
         String model = Build.MODEL;
         int version = Build.VERSION.SDK_INT;
         String versionRelease = Build.VERSION.RELEASE;
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.getDeviceId();
+        PackageInfo pInfo = null;
+        String appVersion="";
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+             appVersion = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         Log.e("MyActivity", "manufacturer " + manufacturer
                 + " \n model " + model
                 + " \n version " + version
                 + " \n versionRelease " + versionRelease
+                + " \n userDeviceId " +telephonyManager.getDeviceId()
+                + " \n Device Language "+  Locale.getDefault().getDisplayLanguage()
+                + " \n Device Country " + Locale.getDefault().getDisplayCountry()
+                + " \n Application Version "+ appVersion
         );
 
-
-        //name=androidDeviceId;
     }
+
+    private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int  health= intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
+            int  icon_small= intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
+            int  level= intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+            int  plugged= intent.getIntExtra(BatteryManager.EXTRA_PLUGGED,0);
+            boolean  present= intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
+            int  scale= intent.getIntExtra(BatteryManager.EXTRA_SCALE,0);
+            int  status= intent.getIntExtra(BatteryManager.EXTRA_STATUS,0);
+            String  technology= intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+            int  temperature= intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0);
+            int  voltage= intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0);
+            Log.e("MyActivity", "health " + health
+                    + " \n icon_small " + icon_small
+                    + " \n level " + level
+                    + " \n plugged " + plugged
+                    + "\n scale " + scale
+                    + "\n status " +status
+                    + "\n technology " +technology
+                    + "\n temperature " +temperature
+                    + "\n voltage " +voltage
+                    + " \n present " +present);
+
+
+
+        }
+    };
 
 }
